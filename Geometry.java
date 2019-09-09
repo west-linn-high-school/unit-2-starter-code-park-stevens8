17ed12b3b924efbef1d7d3842bd99bd30006bc3a
Michael George <georgem@wlwv.k12.or.us>
2019-09-09 09:22:25 -0700

    commit

---
+++ Geometry.java
@@ -0,0 +1,14 @@
+public class Geometry
+{
+    public static void main (String[] args) {
+        int sides = 7;
+        int radius = 5;
+        final double PI = 3.14;
+        
+        System.out.println("A heptagon has " + sides + " sides.");
+        System.out.println("A decagon has " + (radius + 5) + " sides");
+        System.out.println("A dodecagon has " + (sides + radius) + " sides");
+    }
+}    
+
+    
